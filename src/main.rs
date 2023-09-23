2446591c4af8351f76abf2d454d65825d815fe03
Jeff <bitcloud22@gmail.com>
2023-09-23 00:51:34 -0700

    port from rocket to axum

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,12 @@
-#[macro_use]
-extern crate rocket;
-
-use log::info;
-use rocket::fairing::AdHoc;
-use rocket::http::Status;
-use rocket::serde::{json::Json, Deserialize};
-use serde::Serialize;
+use axum::{
+    routing::{get, post},
+    http::StatusCode,
+    Json, Router,
+};
+use serde::{Deserialize, Serialize};
 use serde_json::{Value};
 use std::collections::HashMap;
-use std::env;
+use std::net::SocketAddr;
 
 mod logic;
 
@@ -56,14 +54,31 @@ pub struct GameState {
     board: Board,
     you: Battlesnake,
 }
+#[tokio::main]
+async fn main() {
+    // build our application with a route
+    let app = Router::new()
+        // `GET /` goes to `handle_index`
+        .route("/", get(handle_index))
+        // `POST /start` goes to `handle_start`
+        .route("/start", post(handle_start))
+        .route("/move", post(handle_move))
+        .route("/end", post(handle_end));
 
-#[get("/")]
-fn handle_index() -> Json<Value> {
+    let addr = SocketAddr::from(([0, 0, 0, 0], 8000));
+    println!("Starting BattleSnake Server...");
+    println!("listening on {}", addr);
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn handle_index() -> Json<Value> {
     Json(logic::info())
 }
 
-#[post("/start", format = "json", data = "<start_req>")]
-fn handle_start(start_req: Json<GameState>) -> Status {
+async fn handle_start(Json(start_req): Json<GameState>) -> StatusCode {
     logic::start(
         &start_req.game,
         &start_req.turn,
@@ -71,11 +86,10 @@ fn handle_start(start_req: Json<GameState>) -> Status {
         &start_req.you,
     );
 
-    Status::Ok
+    StatusCode::OK
 }
 
-#[post("/move", format = "json", data = "<move_req>")]
-fn handle_move(move_req: Json<GameState>) -> Json<Value> {
+async fn handle_move(Json(move_req): Json<GameState>) -> Json<Value> {
     let response = logic::get_move(
         &move_req.game,
         &move_req.turn,
@@ -86,40 +100,8 @@ fn handle_move(move_req: Json<GameState>) -> Json<Value> {
     Json(response)
 }
 
-#[post("/end", format = "json", data = "<end_req>")]
-fn handle_end(end_req: Json<GameState>) -> Status {
+async fn handle_end(Json(end_req): Json<GameState>) -> StatusCode {
     logic::end(&end_req.game, &end_req.turn, &end_req.board, &end_req.you);
 
-    Status::Ok
-}
-
-#[launch]
-fn rocket() -> _ {
-    // Lots of web hosting services expect you to bind to the port specified by the `PORT`
-    // environment variable. However, Rocket looks at the `ROCKET_PORT` environment variable.
-    // If we find a value for `PORT`, we set `ROCKET_PORT` to that value.
-    if let Ok(port) = env::var("PORT") {
-        env::set_var("ROCKET_PORT", &port);
-    }
-
-    // We default to 'info' level logging. But if the `RUST_LOG` environment variable is set,
-    // we keep that value instead.
-    if env::var("RUST_LOG").is_err() {
-        env::set_var("RUST_LOG", "info");
-    }
-
-    env_logger::init();
-
-    info!("Starting Battlesnake Server...");
-
-    rocket::build()
-        .attach(AdHoc::on_response("Server ID Middleware", |_, res| {
-            Box::pin(async move {
-                res.set_raw_header("Server", "battlesnake/github/starter-snake-rust");
-            })
-        }))
-        .mount(
-            "/",
-            routes![handle_index, handle_start, handle_move, handle_end],
-        )
+    StatusCode::OK
 }
